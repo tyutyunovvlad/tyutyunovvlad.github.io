355085ff0078c52393deb1cea54c6d2f7f5b78bd
Vlad <fildranger@gmail.com>
2019-06-28 13:11:54 +0300

    keksby resp v1

--- project-Keksby/script/script.js
+++ project-Keksby/script/script.js
@@ -3,9 +3,16 @@ const checkBox = document.querySelector(".check__input"),
     hamburger = document.querySelector(".header__hamburger"),
     menu = document.querySelector(".header__menu"),
     headerButton = document.querySelector(".header__button"),
-    logo = document.querySelector(".header__logo");
+    logo = document.querySelector(".header__logo"),
+    mMenu = document.querySelector(".m-menu"),
+    item1 = document.querySelector("#item1"),
+    item2 = document.querySelector("#item2"),
+    item3 = document.querySelector("#item3");
+let startingX;
 
-    
+
+
+
 checkSpan.addEventListener('click', () => {
     if (!checkBox.hasAttribute("checked")) {
         checkBox.setAttribute("checked", '');
@@ -18,19 +25,86 @@ let menuOpened = false;
 
 hamburger.addEventListener('click', () => {
     if (menuOpened === false) {
-        logo.style.opacity = '0';
-        logo.style.transform ="translate(200px, 0)";
+        logo.style.transform ="translate(300px, 0)";
         menuOpened = true;
         hamburger.style.transform = 'rotate(90deg)';
-        menu.style.transform ="translate(-80px, 0)";
-        headerButton.style.transform ="translate(25px,0)";
+        mMenu.style.transform = 'translateX(10%)';
+
     } else {
         logo.style.display = 'block';
         menuOpened = false;
         hamburger.style.transform = 'rotate(0deg)';
-        logo.style.opacity = '1';
         logo.style.transform ="translate(0, 0)";
-        menu.style.transform ="translate(-70px, -100px)";
-        headerButton.style.transform ="translate(50px,-100px)";
+        mMenu.style.transform = 'translateX(-200%)';
     }
 });
+
+function i1handleTouchStart(evt) {
+    startingX = evt.touches[0].clientX;
+    item1.style.transition = '';
+    item2.style.transition = '';
+};
+function i1handleTouchMove(evt) {
+    let touch = evt.touches[0].clientX;
+    let change = startingX - touch;     
+    if (change < 0) {
+        return;
+    }
+    item1.style.left = '-' + change + 'px';
+    item2.style.left =  (screen.width*32/50 - change) + 'px';
+    evt.preventDefault();
+};
+function i1handleTouchEnd(evt) {
+    let change = startingX - evt.changedTouches[0].clientX;
+    let half = screen.width / 3;
+    if (change < half) {
+        item1.style.left = '0';
+        item2.style.left='80%';
+    } else {
+        item1.style.transition = "all ease-in-out 0.3s";
+        item2.style.transition = "all ease-in-out 0.3s";
+        item1.style.left = "-60%";
+        item2.style.left = "20%";
+    }    
+};
+
+function i2handleTouchStart(evt) {
+    startingX = evt.touches[0].clientX;
+    item1.style.transition = '';
+    item2.style.transition = '';
+}
+function i2handleTouchMove(evt) {
+    let touch = evt.touches[0].clientX;
+    let change = touch - startingX;
+    if (change < 0) {
+        return;
+    }
+    item1.style.left = (change - screen.width) + 'px';
+    item2.style.left = change + 'px';
+    evt.preventDefault();
+}
+function i2handleTouchEnd(evt) {
+    let change = evt.changedTouches[0].clientX - startingX;
+    let half = screen.width / 4;
+    if (change < half) {
+        item1.style.left = '-100%';
+        item2.style.left = '0';
+    } else {
+        item1.style.transition = 'all .3s';
+        item2.style.transition = 'all .3s';
+        item1.style.left = '0';
+        item2.style.left = '100%';
+    }
+}
+
+
+
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
